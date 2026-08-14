f1e7f296afd3eadbfc91f8a80b16d4e625e7a220
Matin <matinyarzadeh13@gmail.com>
2026-08-14 15:39:57 +0430

    fix(hud): correct logger string format format specifier to prevent crashes

--- src/core/game/hud.cpp
+++ src/core/game/hud.cpp
@@ -1,6 +1,5 @@
 #include "hud.h"
-
-#include "core/logging/logger.hpp"
+#include "core/logging/logger.h"
 
 using namespace GTASA::SDK;
 
@@ -21,7 +20,8 @@ void HUD::sendHelpMessage(const std::string& msg)
         LOG_DEBUG("[HUD] sendHelpMessage function resolved.");
     }
 
-    LOG_DEBUG("[HUD] Sending help message: {}", msg);
+    # FIX: Changed formatting to %s and converted std::string using msg.c_str() to prevent runtime crashes
+    LOG_DEBUG("[HUD] Sending help message: %s", msg.c_str());
 
     Internal::Functions::CHUD::sendHelpMessage(msg.c_str(), false, false, false);
 }
